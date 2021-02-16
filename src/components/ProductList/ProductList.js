d88e6ebe6891ae3561e9f5958d582ee0d210a4c3
iyobo <phil.eki@gmail.com>
2021-02-16 10:17:13 -0600

    frontend Checkpoint

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Product } from "..";
+import results from '../../data/products.json';
 
 const ProductList = ({ products, compare, nameSearch }) => {
   const renderProducts = () => {
     return products;
   };
 
+  const [displayedProducts, setDisplayedProducts] = useState(products);
+
+  useEffect(() => {
+    if(!nameSearch) {
+      setDisplayedProducts(products)
+    }else {
+      //filter when namesearch changes
+      const newItems = displayedProducts.filter((it) => it.name.toLowerCase().indexOf(nameSearch.toLowerCase()>-1) || it.description.toLowerCase().includes(nameSearch.toLowerCase()));
+      setDisplayedProducts(newItems);
+    }
+  }, [nameSearch]);
+
   return (
     <div className="row mt-3">
-      {renderProducts().map((product) => (
+      {displayedProducts.map((product) => (
         <Product key={product.id} product={product} compare={compare} />
       ))}
     </div>
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,58 +1,56 @@
-import React, { Component } from "react";
-import { CompareTable, ProductList } from "../../components";
+import React, {useState, useEffect} from "react";
+import {CompareTable, ProductList} from "../../components";
 import results from "../../data/products.json";
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      nameSearch: "",
-      products: results.products,
-    };
-  }
+function Home(props) {
+    const originalProducts = results.products;
+    const [nameSearch, setNameSearch] = useState('');
+    const [displayedProducts, setDisplayedProducts] = useState(results.products);
 
-  render() {
-    const compareProducts = this.state.products.filter(
-      (product) => product.compare
-    );
     const handleChange = (event) => {
-      this.setState({ nameSearch: event.target.value });
+        setNameSearch(event.target.value);
     };
+
+
+
 
     const compare = (id) => {
-      this.setState({
-        products: this.state.products.map((product) =>
-          product.id === id
-            ? { ...product, compare: !product.compare }
-            : product
-        ),
-      });
+        setDisplayedProducts(displayedProducts.map((product) =>
+            product.id === id
+                ? {...product, compare: !product.compare}
+                : product
+        ));
     };
 
-    return (
-      <div className="home mt-5">
-        <div className="row">
-          <div className="col-12">
-            <h2 className="mb-3">Compare Products</h2>
-            <input
-              type="text"
-              placeholder="Search by name"
-              value={this.state.nameSearch}
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
-        </div>
-        <ProductList
-          products={this.state.products}
-          nameSearch={this.state.nameSearch}
-          compare={compare}
-        />
-        {compareProducts.length >= 1 && (
-          <CompareTable products={compareProducts} />
-        )}
-      </div>
+    const compareProducts = displayedProducts.filter(
+        (product) => product.compare
     );
-  }
+
+
+    return (
+        <div className="home mt-5">
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="mb-3">Compare Products</h2>
+                    <input
+                        type="text"
+                        placeholder="Search by name"
+                        value={nameSearch}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </div>
+            </div>
+            <ProductList
+                products={displayedProducts}
+                nameSearch={nameSearch}
+                compare={compare}
+            />
+            {compareProducts.length >= 1 && (
+                <CompareTable products={compareProducts}/>
+            )}
+        </div>
+    );
+
 }
 
 export default Home;
